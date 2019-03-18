5c8e885141e1ff9a05ba6e566714e921bfe4746b
Ibrahim Mizi <ibmiz43@gmail.com>
2019-03-18 21:11:07 +0000

    Added in categories and changed theme

--- pages/details.vue
+++ pages/details.vue
@@ -1,42 +1,63 @@
 <template>
-<div>
-   <b-navbar type="dark" variant="dark"  fixed="top">
-  <b-nav>
-    <b-nav-item active router-link to="/">Home</b-nav-item>
-  </b-nav>
-   </b-navbar>
+  <div>
+    <b-navbar type="dark" variant="dark" fixed="top">
+      <b-nav>
+        <b-nav-item active router-link to="/">Home</b-nav-item>
+      </b-nav>
+    </b-navbar>
 
- <div class="details">
-    <b-card
-        border-variant="primary"
+    <div class="details">
+      <b-card 
+        bg-variant="dark"
+        text-variant="white"
         header="Primary"
         header-bg-variant="primary"
         header-text-variant="white"
         align="center"
-        :title=this.$route.params.crypto_name
+        :title="Id"
       >
-        <b-card-text>Current price is : ${{this.$route.params.price_usd}}</b-card-text>
-        <b-card-text>Percentage change is : {{this.$route.params.percent_change_1h}}</b-card-text>
+        <b-card-text>Current price is: ${{price_usd}}</b-card-text>
+        <b-card-text>Percentage change is: {{percent_change_1h}}%</b-card-text>
       </b-card>
- </div>
 
- </div>
+      <h2 class="text-white bg-dark, text-center"> Next 3 coins</h2>
+
+      <div
+        id="cards"
+        v-for="currency in nextCurrency"
+        :key="currency.name"
+        class="col-md-6"
+      >
+        <b-card bg-variant="dark" :header="currency.name" text-variant="white" class="text-center">
+          <b-card-text>Current price is: ${{currency.price_usd}}</b-card-text>
+          <b-card-text>Percentage change is: {{currency.percent_change_1h}}%</b-card-text>
+          </b-card>
+      </div>
+    </div>
+
+
+
+  </div>
+
+
 </template>
 
 <script lang="ts">
-import {Component, Vue} from "nuxt-property-decorator"
+import { Component, Vue } from "nuxt-property-decorator";
 
- export default Vue.extend({
-  name:'details',
-  data (){
-   return{
-    Id:this.$route.params.crypto_name,
-    title:'Details'
-   }
+export default Vue.extend({
+  name: "details",
+  data() {
+    return {
+      Id: this.$route.params.crypto_name,
+      price_usd: this.$route.params.price_usd,
+      percent_change_1h: this.$route.params.percent_change_1h,
+      nextCurrency: this.$route.params.next_currency,
+      title: "Details"
+    };
   }
- })
+});
 </script>
 
 <style scoped>
- 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,125 @@
-<template>
-  <div>
-      <b-navbar type="dark" variant="dark" fixed="top" static>
-        <b-nav>
-          <b-nav-item active router-link to="/">Home</b-nav-item>
-          <b-nav-item-dropdown text="Categories" right>
-            <b-dropdown-item href="#">Name(ASC)</b-dropdown-item>
-            <b-dropdown-item href="#">Name(DESC)</b-dropdown-item>
-          </b-nav-item-dropdown>
-          <b-nav-item active router-link to="/random">Random</b-nav-item>
-            <select v-model="currentOrder">
-            <option value="rank">Sort by Rank</option>
-            <option value="name">Sort by Name</option>
-                </select>
-        </b-nav>
-      </b-navbar>
-      <div id="cards" v-for="currency in orderedItems(currencies)" :key="currency.name">
+<template >
+  <div class="navigation">
+    <b-navbar type="dark" variant="dark" fixed="top" static>
+      <b-nav>
+        <b-nav-item active router-link to="/">Home</b-nav-item>
+        <b-nav-item active router-link to="/random">Random</b-nav-item>
+      </b-nav>
 
-  <div>
-    <div class="card text-center">
-      <div class="card-header">
+ 
+    <div class="col-xs-3">
+       <b-form-select class="selectpicker" size="sm" ref="Range" v-model="Range">
+          <option value=0>Select category</option>
+          <option value=1>0-10</option>
+          <option value=2>10-20</option>
+        </b-form-select>
+
       </div>
-      <div class="card-body">
-        <h5 class="card-title">{{currency.name}}</h5>
-        <div id="app">
-          <b-button @click="goTodetail(currency.name, currency.price_usd, currency.percent_change_1h)">Details</b-button>
-        </div>
+    </b-navbar>
+
+  <div class="form-group col-md-2">
+    <b-form-select class="selectpicker" ref="currentOrder" v-model="currentOrder">
+      <option value="rank">Sort by Rank</option>
+      <option value="name">Sort by Name</option>
+    </b-form-select>
+  </div>
+
+    <div class="row">
+      <div
+        id="cards"
+        v-for="(currency, index) in orderedItems(returnRange(currencies))"
+        :key="currency.name"
+        class="col-md-6"
+      >
+        <b-card bg-variant="dark" :header="currency.name" text-variant="white" class="text-center">
+          <div id="app">
+            <b-button
+              @click="goTodetail(currency.name, currency.price_usd, currency.percent_change_1h, currencies.slice(index+1,index+4))"
+            >Details</b-button>
+          </div>
+        </b-card>
       </div>
     </div>
   </div>
-
-  </div>
-
-  
-  </div>
-
 </template>
 
 <script lang="ts">
-import {Component, Vue} from "nuxt-property-decorator"
-import axios from 'axios'
-import router from 'vue-router'
+import { Component, Vue } from "nuxt-property-decorator";
+import axios from "axios";
+import router from "vue-router";
 
-export default Vue.extend( {
-
-data () {
+export default Vue.extend({
+  data() {
     return {
-      currentOrder: 'rank',
-      currencies : []
+      currentOrder: "rank",
+      Range: 0,
+      currencies: []
+    };
+  },
+  mounted: function() {
+    axios({
+      method: "GET",
+      url: "https://api.coinmarketcap.com/v1/ticker/?limit=100"
+    }).then(
+      result => {
+        this.currencies = result.data;
+        console.log(result.data);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  },
+  methods: {
+    toggleOrder(name: any) {
+      this.currentOrder = name;
+    },
+    orderedItems(items: any) {
+      return items.sort((a: any, b: any) =>
+        a[this.currentOrder] > b[this.currentOrder] ? 1 : -1
+      );
+    },
+
+    toggleRange(range: any) {
+      this.Range = range;
+    },
+    returnRange(currencies:any) {
+      if(this.Range == 0){
+        return currencies.slice(0,0);
+      }
+      if (this.Range == 1){
+        return currencies.slice(0,10);
+      }
+      if (this.Range == 2){
+        return currencies.slice(11,20)
+      }
+    },
+    goTodetail(Id: any, price: any, percentChange: any, nextCurrency: any) {
+      this.$router.push({
+        name: "details",
+        params: {
+          crypto_name: Id,
+          price_usd: price,
+          percent_change_1h: percentChange,
+          next_currency: nextCurrency
+        }
+      });
     }
   },
-  mounted : function(){
-    axios({ method: "GET", "url": "https://api.coinmarketcap.com/v1/ticker/?limit=10" })
-    .then(result => {
-        this.currencies = result.data;
-    }, error => {
-        console.error(error);
-    })
-  },
- methods: {
-        toggleOrder(name: any) {
-            this.currentOrder = name;
-        },
-        orderedItems(items: any) {
-          return items.sort((a:any, b:any) => (a[this.currentOrder] > b[this.currentOrder]) ? 1 : -1)  
-        },
-          goTodetail(Id:any, price:any, percentChange:any) {
-    this.$router.push({name:'details',params:{crypto_name:Id, price_usd:price, percent_change_1h:percentChange}})
-  } 
-    },
-  props: ["name", "price_usd", "percent_change_1h"]
-
-})
+  props: ["name", "price_usd", "percent_change_1h", "next_currency"]
+});
 </script>
 
-<style scoped>
+<style>
 #cards {
   margin: auto;
   padding: 40px;
 }
-body { padding: 70px; }
+.navigation {
+  padding-top: 70px;
+}
+
+body {
+  background: black !important;
+}
 </style>
